bd92b2133e8c90cafbaa22b995e9374c37fcf444
Ashishyoyo91 <ashishyoyo91@gmail.com>
2025-10-27 15:19:46 +0530

    #

--- resources/views/chooseTemplate.blade.php
+++ resources/views/chooseTemplate.blade.php
@@ -107,31 +107,10 @@
                         </div>
                     </div>
                 </section>
-
-                <div class="row ">
-                    @foreach ($getTepmpate as $item)
-                        <div class="col-md-3">
-                            <div class="card" style="width: 18rem;">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $item['name'] }}</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the
-                                        bulk of
-                                        the card’s content.</p>
-                                    <a href="{{ route('showTemplate', $item['id']) }}" class="btn btn-primary">Go
-                                        Template</a>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
             </div>
         </div>
     </div>
-
-
-
-
+    
 @endsection
 
 
